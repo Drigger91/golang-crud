01e2d2870ea41dc17612dc4040eeebfb28df448c
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2024-09-16 00:45:28 +0530

    database connectivity

--- infrastructure/database/db.go
+++ infrastructure/database/db.go
@@ -12,35 +12,8 @@ import (
 
 var db *gorm.DB
 
-// getDatabase returns the instance of the connected database
-// createDatabase checks if the database exists and creates it if it doesn't
-func createDatabase(dbname, user, password, host string, port int) error {
-	// Connect to the default `postgres` database
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%d sslmode=disable", host, user, password, port)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return fmt.Errorf("failed to connect to default postgres database: %v", err)
-	}
-
-	// Check if the target database exists, and create it if not
-	query := fmt.Sprintf("CREATE DATABASE %s;", dbname)
-	if err := db.Exec("SELECT 1 FROM pg_database WHERE datname = ?", dbname).Error; err != nil {
-		// If the query fails, create the database
-		if err := db.Exec(query).Error; err != nil {
-			return fmt.Errorf("failed to create database %s: %v", dbname, err)
-		}
-		fmt.Printf("Database %s created successfully.\n", dbname)
-	} else {
-		fmt.Printf("Database %s already exists.\n", dbname)
-	}
-
-	return nil
-}
-
 // ConnectToDatabase initializes the connection to the target database
 func ConnectToDatabase() {
-	// Load environment variables from .env
-
 	// Get the database connection details from environment variables
 	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
@@ -52,10 +25,7 @@ func ConnectToDatabase() {
 	}
 	timezone := os.Getenv("DB_TIMEZONE")
 
-	// Try to create the database if it doesn't exist
-	if err := createDatabase(dbname, user, password, host, port); err != nil {
-		log.Fatalf("Failed to create database: %v", err)
-	}
+	fmt.Println("DEBUG DB CONFIG: " + password + " " + host + " " + user)
 
 	// Now connect to the target database
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=%s", host, user, password, dbname, port, timezone)
